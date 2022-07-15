98f44568b9f020eb125902373baaf8b1a9108cac
aliarsalan <aliarsalan.dev@gmail.com>
2022-07-16 03:18:24 +0500

    faq page updated.

--- frontend/src/sections/HeroSection.js
+++ frontend/src/sections/HeroSection.js
@@ -1,24 +1,24 @@
-import React from "react";
-import propTypes from "prop-types";
-import SearchBox from "../components/SearchBox";
+import React from 'react';
+import propTypes from 'prop-types';
+import SearchBox from '../components/SearchBox';
 
 function Hero({
-  heading = "The Art Of FIlm",
-  heading2 = "A MOVIE POSTER COLLECTOR’S INDISPENSABLE TOOLKIT",
-  image = "",
+  heading = 'The Art Of FIlm',
+  heading2 = 'A MOVIE POSTER COLLECTOR’S INDISPENSABLE TOOLKIT',
+  image = '',
   showSearch = false,
 }) {
   return (
     <div
       className="section fh p-2"
       style={{
-        position: "relative",
+        position: 'relative',
         backgroundImage: `url(${image})`,
       }}
     >
-      <h1 className={"title"}>{heading}</h1>
+      <h1 className={'selection'}>{heading}</h1>
       {heading2 && (
-        <h2 className={"text-center text-md"} style={{ margin: "0 auto" }}>
+        <h2 className={'text-center text-md'} style={{ margin: '0 auto' }}>
           {heading2}
         </h2>
       )}
@@ -35,9 +35,9 @@ function Hero({
 }
 
 Hero.defaultProps = {
-  heading: "The Art Of Film",
-  heading2: "A MOVIE POSTER COLLECTOR’S INDISPENSABLE TOOLKIT",
-  image: "",
+  heading: 'The Art Of Film',
+  heading2: 'A MOVIE POSTER COLLECTOR’S INDISPENSABLE TOOLKIT',
+  image: '',
   showSearch: false,
 };
 Hero.propTypes = {
